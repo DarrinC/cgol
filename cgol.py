9c815784b346d0644be4c75cdab4d9679d67a445
DarrinC_on_Github <chocko@gmail.com>
2021-04-14 02:38:12 +0100

    strip directory path to pattern_files/

--- cgol.py
+++ cgol.py
@@ -400,7 +400,7 @@ def edit_field(ifield, patterns, size):
 
 def read_pattern_files():
     import os.path as path
-    pattern_files_directory = 'C:\\Users\\me\\WORK_Non-Music\\IT\\python\\projects\\conway_game_of_life\\pattern_files\\conwaylife.com'
+    pattern_files_directory = 'pattern_files\\conwaylife.com'
 
     pattern_file_list = (
         'blinker.cells',
